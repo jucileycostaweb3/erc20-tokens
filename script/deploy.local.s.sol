8a7f63720579832a0d37687aa45afe166fe79eb2
juciley.costa.web3 <juciley.costa.web3@gmail.com>
2025-04-30 17:17:58 -0300

    Formatting deploy script.

--- script/deploy.local.s.sol
+++ script/deploy.local.s.sol
@@ -5,11 +5,11 @@ import { Script, console2 } from "forge-std/Script.sol";
 import { ZKVerifyToken } from "../src/ZKVerifyToken.sol";
 
 contract LocalScript is Script {
-    function run() external returns(ZKVerifyToken){
+    function run() external returns (ZKVerifyToken) {
         vm.startBroadcast();
-        
+
         ZKVerifyToken _token = new ZKVerifyToken();
-        
+
         vm.stopBroadcast();
         return _token;
     }

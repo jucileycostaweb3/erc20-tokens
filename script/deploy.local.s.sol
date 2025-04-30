dade47cf309f917a78c67766002e50dd291ba3c1
juciley.costa.web3 <juciley.costa.web3@gmail.com>
2025-04-30 17:13:36 -0300

    Refactoring the deploy script.

--- script/deploy.local.s.sol
+++ script/deploy.local.s.sol
@@ -4,17 +4,13 @@ pragma solidity ^0.8.26;
 import { Script, console2 } from "forge-std/Script.sol";
 import { ZKVerifyToken } from "../src/ZKVerifyToken.sol";
 
-contract Local is Script {
-    ZKVerifyToken public _token;
-
-    function setUp() public { }
-
-    function run() public {
-        vm.startBroadcast(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80);
-
-        _token = new ZKVerifyToken();
-        console2.log("ZKVerifyToken(ZKV) address: ", address(_token));
-
+contract LocalScript is Script {
+    function run() external returns(ZKVerifyToken){
+        vm.startBroadcast();
+        
+        ZKVerifyToken _token = new ZKVerifyToken();
+        
         vm.stopBroadcast();
+        return _token;
     }
 }
